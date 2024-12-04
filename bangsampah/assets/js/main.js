418996fec6c2bd06d267a445bd04f99a7a833322
KAROL EFRIDO SANGGAM PURBA <144148238+KEvisual@users.noreply.github.com>
2024-12-04 12:14:16 +0700

    Add files via upload

---
+++ bangsampah/assets/js/main.js
@@ -0,0 +1,5 @@
+// JavaScript untuk toggle menu
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active'); // Toggle class "active" untuk menampilkan atau menyembunyikan menu
+}
